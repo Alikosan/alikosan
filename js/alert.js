475e32bd94b8596108a5551d59792323687c5337
Alikosan <44752555+Alikosan@users.noreply.github.com>
2020-01-21 02:31:18 +0500

    File for site

---
+++ js/alert.js
@@ -0,0 +1,35 @@
+/*let admin, name; // можно объявить две переменные через запятую
+
+name = "Джон";
+
+admin = name;
+
+alert( admin ); // "Джон"  */
+
+
+/*let message = "Hello";
+message = 123456;
+alert( message );*/
+
+/*let name = "Alik";
+alert ( `Hi, ${name}!` );*/
+
+/*let name ="Ilya";
+alert( `hello ${1}` ); // ?*/
+
+
+/*"" + 1 + 0 // 10
+"" - 1 + 0 // 10
+true + false
+6 / "3"
+"2" * "3"
+4 + 5 + "px"
+"$" + 4 + 5
+"4" - 2
+"4px" - 2
+7 / 0
+"  -9  " + 5
+"  -9  " - 5
+null + 1
+undefined + 1
+" \t \n" - 2*/
